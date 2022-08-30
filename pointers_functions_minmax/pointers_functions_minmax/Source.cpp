a5efd6adabd92ef4cbebf2b4a2e93cf8a9c0a7f0
jigar bhanderi <41480945+jigar2099@users.noreply.github.com>
2022-08-30 14:12:16 +0100

    Add files via upload

---
+++ pointers_functions_minmax/pointers_functions_minmax/Source.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+
+int getMin(int numbers[], int size) {
+	int min = numbers[0];
+	for (int i = 1; i < size; i++) {
+		if (numbers[i] < min)
+			min = numbers[i];
+	}
+	return min;
+}
+int getMax(int numbers[], int size) {
+	int max = numbers[0];
+	for (int i = 0; i < size; i++) {
+		if (numbers[i] > max)
+			max = numbers[i];
+	}
+	return max;
+}
+
+void getMinMax(int numbers[], int size, int* min, int* max) {
+	for (int i = 0; i < size; i++) {
+		if (numbers[i] > *max)
+			*max = numbers[i];
+		if (numbers[i] < *min)
+			*min = numbers[i];
+	}
+}
+
+
+int main() {
+	int numbers[5] = { -1,-3,-4,5,6 };
+	//cout << "Min is :" << getMin(numbers, 5)<<endl;
+	//cout << "Max is : " << getMax(numbers, 5)<<endl;
+	int min = numbers[0];
+	int max = numbers[0];
+	getMinMax(numbers, 5, &min, &max);
+	cout << "Min is : " << min << " and Max is : " << max << endl;
+
+	return 0;
+}
